8248bab42a55ad6f7a5c6262eb1ad11acc8b394a
Molo Munyansanga <molomunyansanga@gmail.com>
2025-08-23 20:57:46 +0200

    Added claim submission form component

---
+++ src/components/ClaimsForm.jsx
@@ -0,0 +1,39 @@
+export default function ClaimsForm() {
+    return (
+        <section className="w-130 border border-gray-400 shadow-md p-5 flex flex-col gap-3 rounded-sm">
+            <h1 className="font-semibold text-2xl">Claim Submission Form</h1>
+            <p className="text-gray-600">Fill out the form below to submit a new insurance claim.</p>
+            <form className="flex flex-col gap-4" action="">
+                <div className="input_item">
+                    <label htmlFor="">Policy Number</label>
+                    <input type="text" name="policyNumber" id="policyNumber" />
+                </div><div className="input_item">
+                    <label htmlFor="">Incident Type</label>
+                    <select className="border p-2" name="incidentType" id="incidentType">
+                        <option disabled>Select incident type</option>
+                        <option value="car">Car</option>
+                        <option value="health">Health</option>
+                        <option value="property">Property</option>
+                        <option value="other">Other</option>
+                    </select>
+                </div>
+                <div className="input_item">
+                    <label htmlFor="">Incident Date</label>
+                    <input type="date" name="incidentDate" id="incidentDate" />
+                </div>
+                <div className="input_item">
+                    <label htmlFor="">Description</label>
+                    <textarea className="border" name="description" id="description" rows={3}></textarea>
+                </div>
+                <div className="input_file flex flex-col">
+                    <label htmlFor="">Upload Attachment</label>
+                    <input type="file" name="upload" id="upload" />
+                </div>
+                <div className="form_buttons w-full flex justify-end gap-3">
+                    <button className="border-gray-400 hover:bg-gray-100">Reset</button>
+                    <button className="bg-sky-700 hover:bg-sky-800 text-white border-blue-900">Submit</button>
+                </div>
+            </form>
+        </section>
+    )
+}
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,5 @@
 import Header from "../components/header"
 import Footer from "../components/footer"
-import Dashboard from "./DashboardPage";
 import { useNavigate } from 'react-router-dom'
 
 export default function LandingPage() {
@@ -17,7 +16,7 @@ export default function LandingPage() {
                 <h1 className='font-bold text-2xl sm:text-5xl'>Simplify Insurance Claims Tracking</h1>
                 <h2 className='text-2xl'>Submit claims, track progress, and stay updated in real time.</h2>
                 <div className="buttons mt-5 flex gap-5">
-                    <button onClick={handleClick} className="py-2 w-40 cursor-pointer font-bold rounded-xl border bg-sky-700 hover:bg-sky-800 text-white border border-blue-900">Login as User</button>
+                    <button onClick={handleClick} className="py-2 w-40 cursor-pointer font-bold rounded-xl border bg-sky-700 hover:bg-sky-800 text-white border-blue-900">Login as User</button>
                     <button onClick={handleClick} className="py-2 w-40 cursor-pointer font-bold rounded-xl border border-gray-400 hover:bg-gray-100">Login as Agent</button>
                 </div>
                 </div>
