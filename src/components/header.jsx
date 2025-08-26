686b9d43d5148c3f10e37bbee73066d52e8578f0
Molo Munyansanga <molomunyansanga@gmail.com>
2025-08-26 15:54:50 +0200

    Removed header component since I was using different header designs per page

--- src/components/header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-    return (
-        <header className="flex gap-2 items-center ml-3 sm:ml-58 pt-2 pb-3 sm:pb-10">
-            <img className="w-5" src="./Logo-edited.png" alt="" />
-            <h1 className="font-semibold text-xl">InsurTech Claims Tracker</h1>
-        </header>
-    )
-}
