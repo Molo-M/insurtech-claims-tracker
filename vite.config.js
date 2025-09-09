ed8c830b1171ab5322d3e3a66443be2fedb70042
Molo Munyansanga <molomunyansanga@gmail.com>
2025-09-09 21:02:27 +0200

    Added "/" to base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/insurtech-claims-tracker/",
   plugins: [react(), tailwindcss()],
-  base: "/insurtech-claims-tracker"
 })
