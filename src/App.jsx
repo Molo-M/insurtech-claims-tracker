a8104bccdd43afef30ba79d8d84b8a5db66c9929
Molo Munyansanga <molomunyansanga@gmail.com>
2025-09-09 21:56:20 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,9 @@ export default function App() {
       <Router basename="/insurtech-claims-tracker">
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/index.html" element={<LandingPage />} />
+          <Route path="/insurtech-claims-tracker" element={<LandingPage />} />
+          <Route path="/insurtech-claims-tracker/" element={<LandingPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </Router>
