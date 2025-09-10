92a21aa620d29c59628419e16234d4d0e625f36f
Molo Munyansanga <molomunyansanga@gmail.com>
2025-09-10 15:13:14 +0200

    Updated descriptions to sound more like real life cases

--- src/claimsData.js
+++ src/claimsData.js
@@ -3,7 +3,7 @@ export default [
         policyNum: 123456,
         incidentType: "Car",
         date: "2025-08-18",
-        description: "Delivery truck collided with my truck",
+        description: "Rear-ended by a delivery truck at a stoplight, resulting in minor bumper damage.",
         imgUrl: "crash.jpg",
         status: "Submitted"
     },
@@ -11,7 +11,7 @@ export default [
         policyNum: 7891011,
         incidentType: "Health",
         date: "2025-08-24",
-        description: "Lost my front tooth in a fight",
+        description: "Sustained a broken tooth during a recreational sports accident at a local gym.",
         imgUrl: "man-falling-down-stairs.jpg",
         status: "In Review"
     },
@@ -19,7 +19,7 @@ export default [
         policyNum: 654321,
         incidentType: "Car",
         date: "2025-08-18",
-        description: "Delivery truck collided with my truck",
+        description: "Side collision with another vehicle in a parking lot; minor scratches on the driver-side door.",
         imgUrl: "crash.jpg",
         status: "Approved"
     },
@@ -27,7 +27,7 @@ export default [
         policyNum: 9374651,
         incidentType: "Health",
         date: "2025-08-24",
-        description: "Lost my front tooth in a fight",
+        description: "Slipped on icy pavement outside home, resulting in a sprained ankle.",
         imgUrl: "man-falling-down-stairs.jpg",
         status: "Rejected"
     }
